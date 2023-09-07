e90d2b130cd127c3243155a3c3a2b0b05c5ebae6
Judith <judith@kewomedia.com>
2023-09-07 19:40:07 +0200

    Adelanto de proyecto
    Finalizado efecto encendido de luces.
    Añadimos flecha y efecto chicle.
    Añadimos título al proyecto.

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ Draggable.create("#dragme",{
     type: 'x',
     bounds: '.container',
 })
+
+function checkOverlaop(){
+    var div1 = document.querySelector('.onOff');
+    var div2 = document.getElementById('dragme');
+    var arrow = document.querySelector('.arrow');
+    var rect1 = div1.getBoundingClientRect();
+    var rect2 = div2.getBoundingClientRect();
+
+    if(rect1.left < rect2.right){
+    document.getElementById('video1').style.opacity = 0;
+    div1.style.backgroundColor = "orange";
+    div2.style.color = "transparent";
+
+    arrow.style.transform = "scaleX(0)"; 
+}
+else{
+    document.getElementById('video1').style.opacity = 1;
+    div1.style.backgroundColor = "chartreuse";
+    div2.style.color = "white";
+
+    arrow.style.transform = "scaleX(1)";
+}
+}
+
+window.addEventListener('mousemove', checkOverlaop)
